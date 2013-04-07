17107bf273ba3324c904c51e80d83adcff2958b2
Chirayu Desai <cdesai@cyanogenmod.org>
2013-04-07 17:28:36 +0530

    ril.h: sync RIL_RadioTechnology with framework
    Change-Id: I8e7d4f153aec7b008d465df61a4030549e25de95
    
    Conflicts:
    	include/telephony/ril.h

--- include/telephony/ril.h
+++ include/telephony/ril.h
@@ -165,7 +165,8 @@ typedef enum {
     RADIO_TECH_HSPAP = 15, // HSPA+
     RADIO_TECH_GSM = 16, // Only supports voice
     RADIO_TECH_TD_SCDMA = 17,
-    RADIO_TECH_IWLAN = 18
+    RADIO_TECH_IWLAN = 18,
+    RADIO_TECH_DCHSPAP = 30
 } RIL_RadioTechnology;
 
 // Do we want to split Data from Voice and the use
